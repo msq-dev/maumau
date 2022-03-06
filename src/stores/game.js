44a6932034ae1fb1dfe6515814d3d2edeb664283
msq-dev <maximilian.spuling+github@gmail.com>
2022-03-06 05:59:53 +0100

    improved first move logic

--- src/stores/game.js
+++ src/stores/game.js
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store"
 
-export const sessionRunning = writable(true)
+export const sessionRunning = writable(false)
 export const thePlayers = writable([])
 export const currentCard = writable("")
 export const currentPlayer = writable({})
